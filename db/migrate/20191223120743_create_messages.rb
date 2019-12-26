0ea5ec9a6375514dea7121c553e9990c9974ef61
kirishun <0511shunsuke@gmail.com>
2019-12-26 15:37:27 +0900

    db delete

--- db/migrate/20191223120743_create_messages.rb
+++
@@ -1,9 +0,0 @@
-class CreateMessages < ActiveRecord::Migration[5.0]
-  def change
-    create_table :messages do |t|
-      t.text :body
-      t.string :image
-      t.timestamps null: false
-    end
-  end
-end
--- db/migrate/20191223121453_create_groups.rb
+++
@@ -1,9 +0,0 @@
-class CreateGroups < ActiveRecord::Migration[5.0]
-  def change
-    create_table :groups do |t|
-      t.string :name
-      t.string :password
-      t.timestamps null: false
-    end
-  end
-end
